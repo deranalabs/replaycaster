fe80afa94264e0c14cb9e56bd63228d812fa17b9
deranalabs <dubayu47@gmail.com>
2025-12-05 00:40:19 +0700

    Add cache headers for API

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,19 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  headers: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=60",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;

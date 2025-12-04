72805468ce24384751bbb6e580d0f17e62cc6f8a
deranalabs <dubayu47@gmail.com>
2025-12-05 00:36:29 +0700

    Add hosted manifest redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/.well-known/farcaster.json",
+        destination: "https://api.farcaster.xyz/miniapps/hosted-manifest/019aea6d-f47a-0730-bbf0-217b6de71ef8",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
